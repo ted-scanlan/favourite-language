dff5a82a041bbd373a9d7b08ea5c1aac071aa482
ted_scanlan <t.scanlan92@gmail.com>
2019-09-30 00:16:54 +0100

    test passing for fetchUser function using a mock json object

--- bin/__tests__/fetchUser.test.js
+++ bin/__tests__/fetchUser.test.js
@@ -1,1 +1,18 @@
-const { fetchUser } = require('../src/userSearch');
+const { fetchUser } = require('../src/fetchUser');
+const mockAxios = require("axios")
+const mockObject = require("../__fixtures__/mockObject")
+
+
+
+it("fetches data from api", async () => {
+  // setup
+  mockAxios.get.mockImplementationOnce(() =>
+    Promise.resolve({
+      data: { mockObject }
+    })
+  );
+
+  const repos = await fetchUser('ted-scanlan', "be94e3e8b03a88ca0d4d8cffa06cd6b89de4964c");
+
+expect(repos).toEqual({mockObject})
+});
