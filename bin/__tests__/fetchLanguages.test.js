c5b50f5a3e3f3103026ae5b4bf56580b98996990
ted_scanlan <t.scanlan92@gmail.com>
2019-09-30 00:58:54 +0100

    all tests passing

--- bin/__tests__/fetchLanguages.test.js
+++ bin/__tests__/fetchLanguages.test.js
@@ -14,4 +14,11 @@ it("fetches data from languages api", async () => {
   const languages = await fetchLanguages('ted-scanlan', 'makersb_n_b', "be94e3e8b03a88ca0d4d8cffa06cd6b89de4964c");
 
 expect(languages.data).toEqual({mockObjectLanguages})
+expect(mockAxios.get).toHaveBeenCalledTimes(1);
+expect(mockAxios.get).toHaveBeenCalledWith(
+  "http://api.github.com/repos/ted-scanlan/makersb_n_b/languages", {
+    headers: {
+      Authorization: 'Bearer ' + 'be94e3e8b03a88ca0d4d8cffa06cd6b89de4964c'
+    }
+  })
 })
