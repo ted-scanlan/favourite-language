f82929b079c057cd8e3a93efd3ac0803bc81139b
ted_scanlan <t.scanlan92@gmail.com>
2019-09-30 00:19:28 +0100

    2 more tests for fetchUser function

--- bin/__tests__/fetchUser.test.js
+++ bin/__tests__/fetchUser.test.js
@@ -15,4 +15,14 @@ it("fetches data from api", async () => {
   const repos = await fetchUser('ted-scanlan', "be94e3e8b03a88ca0d4d8cffa06cd6b89de4964c");
 
 expect(repos).toEqual({mockObject})
+expect(mockAxios.get).toHaveBeenCalledTimes(1);
+expect(mockAxios.get).toHaveBeenCalledWith(
+  "http://api.github.com/users/ted-scanlan/repos", {
+    headers: {
+      Authorization: 'Bearer ' + 'be94e3e8b03a88ca0d4d8cffa06cd6b89de4964c'
+    }
+  })
+
+
+
 });
